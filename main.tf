9f26a75394e91de9b5c83f495ebf8b0db3d18489
mhaikh <153711058+mhaikh@users.noreply.github.com>
2023-12-13 08:47:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,7 @@
 resource "aws_s3_bucket" "bucket1" {
-	bucket = "vishnu-first-bucket-19oct"
-}
-
-resource "aws_s3_bucket_acl" "example_bucket_acl" {
-    bucket = aws_s3_bucket.bucket1.id
-    acl = "private"
+	bucket = "mahi-first-bucket-19oct"
 }
 
 provider "aws" {
-    access_key=var.accesskey
-    secret_key=var.secretkey
     region = "eu-west-1"
-}
-
-variable "accesskey"{
-    default="AKIAZTIMJ7JHIX5X6XYT"
-
-}
-
-variable "secretkey"{
-    default="sZxGTgqjwRdnyDHlckv4QzTaY2OVxz1oObnB6ZUM"
 }
